e9ea9fe2c32f35438c0eb3f86ebaf0043afd69c0
ghinzuka <baptiste.pouquerou@gmail.com>
2024-04-16 14:04:42 +0200

    adding the two function of fork

---
+++ exec.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(void)
+{
+	char *argpath = "/bin/ls";
+    char *argv[] = {"/bin/ls", "-l", "/usr/", NULL};
+
+    printf("Before execve\n");
+    if (execve(argpath, argv, NULL) == -1)
+    {
+        perror("Error:");
+        printf("After execve\n");
+    }
+    
+    return (0);
+}
