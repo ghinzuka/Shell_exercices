bc958a868eb8d2f32659e17ce5ecccfbe88979e8
ghinzuka <baptiste.pouquerou@gmail.com>
2024-04-16 09:03:13 +0200

    adding the EOF with an if statement

--- prompt.c
+++ prompt.c
@@ -3,20 +3,19 @@
 
 int main(void)
 {
-	char *buff = malloc(sizeof 512);
-	size_t len = 512;
+	char *buff = malloc(sizeof 1024);
+	size_t len = 1024;
 
 	while (1)
 	{
-		
 			printf("$ ");
 			getline(&buff, &len, stdin);
-			printf("%s", buff);
-		if (getchar() == EOF)
-		{
+			printf("buffer if : %s", buff);
+			if (getchar () == EOF)
+			{
 			printf("CTRL + D pressed \n");
 			printf("the value of EOF is : %d \n", EOF);
 			return (0);
-		}
+			}
 	}
 }
